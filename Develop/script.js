e12ecea6999ff066078f0ba2c29d68ba2758965b
chinda trate <chindatrate@me.com>
2022-01-09 14:55:24 -0500

    added conditionals and loop

--- Develop/script.js
+++ Develop/script.js
@@ -18,6 +18,7 @@ function writePassword() {
 }
 
 function generatePassword() {
+  console.log ("click button")
   userPassword = "";
   passwordCharacters = "";
 
@@ -28,9 +29,7 @@ let pwLength = prompt (
 
 if (pwLength >= 8 && pwLength <= 24) {
   password.length = pwLength;
-} else {
-  alert("Please select a password between 8-24 characters");
-}
+
 
 // Confirm to include lowercase letters
 var isLowerCase = confirm("Click OK to include lowercase letters");
@@ -40,8 +39,30 @@ var isUpperCase = confirm("Click OK to include uppercase letters");
 var isNumeric = confirm("Click OK to include numbers");
 // Confirm to include special characters
 var hasSpecialChars = confirm("Click OK to include special characters");
+} else {
+  alert("Please select a password between 8-24 characters");
 }
 
+// Conditionals
+if (isLowerCase === true) {
+  passwordCharacters += lowercase;
+}
+if (isUpperCase === true) {
+  passwordCharacters += uppercase;
+}
+if (isNumeric === true) {
+  passwordCharacters += numbers;
+}
+if (hasSpecialChars === true) {
+  passwordCharacters += specialCharacters;
+} else {
+  alert ("Error: Cannot generate a password");
+}
+for (var i = 0; i < pwLength; i++) {
+  userPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+}
+return userPassword;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
