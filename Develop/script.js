e9a109854b16b49ccb2d8778e3bb97b519449e9a
chinda trate <chindatrate@me.com>
2022-01-09 13:30:21 -0500

    added requirements, main function and password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define constant variable for password criteria
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const pwLength = "";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +16,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+  userPassword = "";
+  passwordCharacters = "";
+
+// Select length of password
+let pwLength = prompt (
+  "Please select the length of your password. (Between 8-24 characters)"
+);
+
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
